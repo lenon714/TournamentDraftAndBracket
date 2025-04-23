411e7aa5f77c2c7b403b909b7a4be51e49811552
lenon714 <142073542+lenon714@users.noreply.github.com>
2025-04-23 11:06:08 -0400

    Update bracket_app.py

--- bracket_app.py
+++ bracket_app.py
@@ -1,8 +1,8 @@
 from __init__ import *
 
 _component_func = components.declare_component(
-    'bracket',
-    path='streamlit_bracket_app/streamlit_bracket/frontend'
+    "bracket",
+    path=os.path.join(os.path.dirname(__file__), "streamlit_bracket_app/streamlit_bracket/frontend/build")
     # url='http://localhost:3001'
 )
 
